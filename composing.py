17d80f12b7f1e5f3c310ba7b7e3e62ca18081c22
jack911jie <jack911jie@qq.com>
2020-08-30 12:10:42 +0800

    改进判断逻辑

--- composing.py
+++ composing.py
@@ -14,17 +14,26 @@ def char_len(txt):
 def split_txt_Chn_eng(wid,font_size,txt_input,Indent='no'):
     
     def put_words(txts,zi_per_line):    
-
         txtGrp=[]
         wd_lng=0
         pre_txt=''
         for c,t in enumerate(txts):
             if res.match(t):
-                pre_txt=pre_txt+t #汉字
-                wd_lng=wd_lng  #汉字，字数按加1
+                if wd_lng+1>zi_per_line:
+                    txtGrp.append(pre_txt)
+                    pre_txt=t
+                    wd_lng=1
+                else:
+                    pre_txt=pre_txt+t
+                    wd_lng=wd_lng+1
             else:
-                pre_txt=pre_txt+' '+t #英文前加个空格
-                wd_lng=wd_lng+char_len(t) #英文字数按英文字符数计算
+                if wd_lng+char_len(t)>zi_per_line: #先判断是这个英文单词+原有拼接的字符串长度是否>每行字符数
+                    txtGrp.append(pre_txt) #大于，则保持原有的拼接字符串，不再加入该英文单词
+                    pre_txt=' '+t #新的英文单词另起一行
+                    wd_lng=char_len(t) #拼接字符串长度清零重计
+                else:                    
+                    pre_txt=pre_txt+' '+t
+                    wd_lng=wd_lng+char_len(t)
             
             if wd_lng>zi_per_line:
                 wd_lng=0                
@@ -34,7 +43,10 @@ def split_txt_Chn_eng(wid,font_size,txt_input,Indent='no'):
                 if c==len(txts)-1:
                     wd_lng=0                
                     txtGrp.append(pre_txt)
-                    pre_txt=''         
+                    pre_txt='' 
+                    
+                
+        logging.info(txtGrp)
         return txtGrp
     
     txts=txt_input.splitlines()
@@ -44,7 +56,7 @@ def split_txt_Chn_eng(wid,font_size,txt_input,Indent='no'):
         for i,t in enumerate(txts):
             txts[i]=chr(12288)+chr(12288)+t
     
-    res = re.compile(r'([\u4e00-\u9fa5])')   #检测汉字
+    res = re.compile(r'([\u4e00-\u9fa5])')   
     singleTxts=[]
     for t in txts:
         _t=res.split(t)
@@ -61,7 +73,7 @@ def split_txt_Chn_eng(wid,font_size,txt_input,Indent='no'):
                 grp.extend(st.split(' '))
         split_txt.append(grp)
         
-#    logging.info(split_txt)
+    logging.info(split_txt)
         
     
     total_num=0
@@ -73,5 +85,7 @@ def split_txt_Chn_eng(wid,font_size,txt_input,Indent='no'):
     outTxt=[]
     for r,split_t in enumerate(split_txt):
         outTxt.append(put_words(split_t,zi_per_line))
+        
+    
    
     return outTxt 
