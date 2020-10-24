1fc8cf0dced1f86769382a09034b54a20cafea30
jack911jie <jack911jie@hotmail.com>
2020-10-24 20:50:46 +0800

    匹配星期六上课签到表

--- LegoStudentPic/LegoStudentPicDistribute.py
+++ LegoStudentPic/LegoStudentPicDistribute.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.ERROR, format='%(levelname)s | %(funcName)s - 
 logger = logging.getLogger(__name__)
 
 class LegoPics:
-    def __init__(self,crsName):
+    def __init__(self,crsName,weekday=2):
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'StudentsPicConfig.txt'),'r',encoding='utf-8') as f:
             lines=f.readlines()
             _line=''
@@ -24,9 +24,14 @@ class LegoPics:
         self.dir=config['乐高照片文件夹']
         self.stu_dir=config['乐高学员文件夹']
         self.stu_sigDir=config['2020乐高课程签到表文件夹']
+        self.weekday=weekday
 
-    def read_sig(self):
-        stdInfo=pd.read_excel(os.path.join(self.stu_sigDir,'2020乐高课程签到表.xlsx'),sheet_name='学生上课签到表',skiprows=2)
+    def read_sig(self,weekday):
+        if weekday==2:
+            sigFile='2020乐高课程签到表（周二）.xlsx'
+        elif weekday==6:
+            sigFile='2020乐高课程签到表（周六）.xlsx'
+        stdInfo=pd.read_excel(os.path.join(self.stu_sigDir,sigFile),sheet_name='学生上课签到表',skiprows=2)
         stdInfo.rename(columns={'Unnamed: 0':'幼儿园','Unnamed: 1':'班级','Unnamed: 2':'姓名首拼','Unnamed: 3':'性别','Unnamed: 4':'学生姓名','Unnamed: 5':'已上课数'},inplace=True)
         stdName=stdInfo['学生姓名'].tolist()
         stdPY=stdInfo['姓名首拼'].tolist()
@@ -38,13 +43,13 @@ class LegoPics:
         
     def dispatch(self):
         print('将打标签的照片分配到“I:\\每周乐高课_学员”中……')
+        stdInfos=self.read_sig(weekday=self.weekday)
+        dictPY=stdInfos[0]
+        stdNamelist=stdInfos[1]
         for fn in os.listdir(os.path.join(self.dir,self.crsName)):
             if fn[-3:].lower()=='jpg':
 #                 if iptcinfo3.IPTCInfo(os.path.join(self.dir,fn)):
-                stdInfos=self.read_sig()
-                dictPY=stdInfos[0]
-                stdNamelist=stdInfos[1]
-                tag=code_to_str(iptcinfo3.IPTCInfo(os.path.join(self.dir,self.crsName,fn)))                
+                tag=code_to_str(iptcinfo3.IPTCInfo(os.path.join(self.dir,self.crsName,fn)))      
                 if len(tag)>0:
                     for _tag in tag:
                         if _tag in stdNamelist:
@@ -61,7 +66,7 @@ class LegoPics:
                                 shutil.copyfile(oldName,newName)
         
 
-        print('\n……完成')
+        print('\n完成')
 
             
             
@@ -79,5 +84,5 @@ def code_to_str(ss):
 
 
 if __name__=='__main__':
-    stu_pics=LegoPics('20201020啃骨头的小狗')
+    stu_pics=LegoPics('20201024认识零件',weekday=6)
     stu_pics.dispatch()
